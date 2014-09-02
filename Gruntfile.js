ae79176875a379a15a5fbbbe6b3445a5374523d0
Tom Chambers <tom.chambers@gmail.com>
2014-09-02 21:00:01 +0100

    fixed font issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,15 +7,51 @@ module.exports = function(grunt) {
     	files: ['*.js']
     },
     watch: {
-    	files: ['<%= jshint.files %>'],
-    	tasks: ['jshint']
-    }
+      js: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint'],
+        options: {
+          reload: true,
+          livereload: true
+        }        
+      },
+      html: {
+        files: ['*'],
+        options: {
+          reload: true,
+          livereload: true
+        }   
+      }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'app',
+          dest: 'dist',
+          src: ['*']
+        }]
+      }
+    },
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: ['**']
+    },
   });
 
   //load in all the modules
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-gh-pages');  
 
   //register tasks
-  grunt.registerTask('default', ['jshint'])
+  grunt.registerTask('default', ['jshint']);
+
+  grunt.registerTask('build', []);
+
+  grunt.registerTask('serve', ['watch']);
 };
--- app/scripts/script.js
+++ app/scripts/script.js
@@ -2,4 +2,4 @@ var height = $('.portrait').innerHeight();
 $('.portrait').css('width', height);
 $(window).resize(function() {
 	$('.portrait').css('width', height);
-});
+}); 
